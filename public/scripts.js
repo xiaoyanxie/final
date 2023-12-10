4c63b40e743fb5368210c43019f9d7d088f11002
XIAOYAN XIE <122268864+xiaoyanxie@users.noreply.github.com>
2023-12-10 00:06:14 -0500

    Add functionality to display itinerary details
    The commit adds an ID to each itinerary card created. In addition, the 'showDetails' function is updated to fetch and display details about a specific itinerary when its ID is provided. Moreover, the '/api/itineraries' endpoint in the app.js file can now handle a query parameter for an itinerary ID, returning the corresponding itinerary data.

--- public/scripts.js
+++ public/scripts.js
@@ -5,19 +5,20 @@ async function loadItineraries() {
           throw new Error(`HTTP error! Status: ${response.status}`);
       }
       const itineraries = await response.json();
-      console.log('Itineraries:', itineraries); 
+      console.log('Itineraries:', itineraries);
 
       const container = document.getElementById('itinerariesContainer');
-      console.log('Container:', container); 
-      
+      console.log('Container:', container);
+
       itineraries.forEach(itinerary => {
-          console.log('Processing itinerary:', itinerary); 
+          console.log('Processing itinerary:', itinerary);
           const card = document.createElement('div');
+          card.id = itinerary._id;
           card.className = 'itinerary-card';
           card.innerHTML = `
             <h3>${itinerary.name}</h3>
             <p>Created at: ${new Date(itinerary.created_at).toLocaleDateString()}</p>
-            <button onclick="showDetails('${itinerary._id}')">View Details</button>
+            <button onclick="showDetails('${card.id}')">Show Details</button>
         `;
     container.appendChild(card);
       });
@@ -28,7 +29,23 @@ async function loadItineraries() {
 
 
 async function showDetails(itineraryId) {
-  alert('Show details for itinerary ID: ' + itineraryId);
+    try {
+        const response = await fetch(`/api/itineraries?id=${itineraryId}`);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const itinerary = await response.json();
+        console.log('Itinerary:', itinerary)
+        const citiesAndPlacesStr = itinerary.cities.map(city => {
+            const cityStr = `City: ${city.city_id}\nArrival: ${new Date(city.arrival_date).toLocaleDateString()}\nDeparture: ${new Date(city.departure_date).toLocaleDateString()}`;
+            const placesStr = city.places.map(place => `Place: ${place.place_id}\nDay: ${place.day}\nTime: ${place.time}`).join('\n\n');
+            return `${cityStr}\n\n${placesStr}`;
+        }).join('\n\n');
+        // alert(`Details for itinerary ID: ${itineraryId}\nName: ${itinerary.name}\nCreated at: ${new Date(itinerary.created_at).toLocaleDateString()}`);
+        alert(`Details for itinerary ID: ${itineraryId}\nName: ${itinerary.name}\nCreated at: ${new Date(itinerary.created_at).toLocaleDateString()}\n\n${citiesAndPlacesStr}`);
+    } catch (error) {
+        console.error('Fetch error:', error.message);
+    }
 }
 
 loadItineraries();
